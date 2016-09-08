856271b64c2c1dcb256c04a7fa441fb952628ae5
PT <dingfan.pf@gmail.com>
2016-09-08 23:18:18 +0800

    add c-lang folder

---
+++ CodePractice/CodePractice/C-lang/MainC.h
@@ -0,0 +1,13 @@
+//
+//  MainC.h
+//  CodePractice
+//
+//  Created by PointrerTan on 16/9/8.
+//  Copyright © 2016年 PT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MainC : NSObject
+
+@end
---
+++ CodePractice/CodePractice/C-lang/MainC.m
@@ -0,0 +1,15 @@
+//
+//  MainC.m
+//  CodePractice
+//
+//  Created by PointrerTan on 16/9/8.
+//  Copyright © 2016年 PT. All rights reserved.
+//
+
+#import "MainC.h"
+
+@implementation MainC
+
+
+
+@end
